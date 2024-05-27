cd7a664c9bbabe618f0152610ddad66fa5c65028
Virtual Jasper <jasper2virtual@hotmail.com>
2024-05-27 23:53:23 +0800

    bundle style.css

---
+++ src/pages/_hello.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <p class="hello-vue">Hello vue</p>
+</template>
+
+<style scoped>
+.hello-vue{
+  color: red;
+}
+</style>
---
+++ src/pages/_morning.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <p class="morning-vue">Morning vue</p>
+</template>
+
+<style scoped>
+.morning-vue{
+  color: yellow;
+}
+</style>
